7cca14e1f1999ae8b8d5c603d15d714be393d3ab
Cesar Colunga <cesara.colunga@gmail.com>
2023-03-23 11:32:32 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+// Remove the Main using the main ID
+const main = document.getElementById("main");
+main.remove();
+
+// Create newHeader and have it point to the <h1> node
+// give the newHeader the id of "victory"
+//put text of newHeader to be "Cesar is the Champion"
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Cesar is the champion";
